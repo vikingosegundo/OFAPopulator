cc21f8cceed01429db1f0492cd1ac3ffe500f42b
Manuel Meyer <vikingosegundo@gmail.com>
2016-07-21 16:05:27 +0200

    fix selecting

--- OFAPopulator/OFA/OFAViewPopulator.m
+++ OFAPopulator/OFA/OFAViewPopulator.m
@@ -33,7 +33,6 @@
         tv.dataSource = self;
         tv.delegate = self;
         tv.allowsMultipleSelection = YES;
-        tv.allowsMultipleSelectionDuringEditing = YES;
         [tv setEditing:YES animated:YES];
     }
     return self;
